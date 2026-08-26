8c59a029d776dfb1d01ea1f050e1326b9ce7e1e7
whiterose-arch <e.adekojo@alustudent.com>
2026-08-26 15:21:56 +0100

    feat: completed quest 29

--- quests/quest_29_code_breaker.py
+++ quests/quest_29_code_breaker.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Quest 29: The Code Breaker
 ==========================
@@ -5,7 +6,7 @@ Level: Level 6: The Grand Challenge
 
 The Task: Translate the following human logic into a Python script: Allow a user 3 attempts to guess the secret code (42). Give feedback on each guess and stop the game on a correct guess or after 3 failed attempts.
 
-Assigned to : <fill in your name>
+Assigned to : Emmanuel Adekojo
 Group       : group2-python-quests
 Status      : [ ] not started  [ ] in progress  [ ] done  [ ] reviewed by peers
 """
@@ -16,4 +17,15 @@ Status      : [ ] not started  [ ] in progress  [ ] done  [ ] reviewed by peers
 # and add short inline comments (#) explaining tricky lines before you push.
 # ---------------------------------------------------------------------------
 
-# TODO: implement the quest here
+secret_code = 42
+attempts = 0
+while attempts < 3:
+    guess = int(input("Enter your guess: "))
+    if guess == secret_code:
+        print("You guessed the secret code!")
+        break
+    else:
+        print("Wrong guess!")
+    attempts += 1
+if attempts == 3:
+    print("You failed to guess the secret code!")
