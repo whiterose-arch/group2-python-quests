4f991aad5374f6f2a7d53b2547ab699faa8fa6b4
binelintsa-sketch <binelintsa@gmail.com>
2026-08-27 17:55:11 +0400

    completed task 27

--- quests/quest_27_fizzbuzz_test.py
+++ quests/quest_27_fizzbuzz_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Quest 27: The FizzBuzz Test
 ===========================
@@ -5,7 +6,7 @@ Level: Level 6: The Grand Challenge
 
 The Quest: A classic challenge. Print numbers from 1 to 100. For multiples of 3, print "Fizz". For multiples of 5, print "Buzz". For multiples of both, print "FizzBuzz".
 
-Assigned to : <fill in your name>
+Assigned to : Ange Emmanuelle Ntsa Bineli
 Group       : group2-python-quests
 Status      : [ ] not started  [ ] in progress  [ ] done  [ ] reviewed by peers
 """
@@ -16,4 +17,13 @@ Status      : [ ] not started  [ ] in progress  [ ] done  [ ] reviewed by peers
 # and add short inline comments (#) explaining tricky lines before you push.
 # ---------------------------------------------------------------------------
 
-# TODO: implement the quest here
+for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz", i)
+        elif i % 3 == 0:
+                print("Fizz", i)
+
+        elif i % 5 == 0:
+                print("Buzz", i)
+        else:
+                print(i)
