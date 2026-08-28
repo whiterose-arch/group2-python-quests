b0847eff8ad6b067c3fb3789fa0d2445f55bd230
binelintsa-sketch <binelintsa@gmail.com>
2026-08-28 21:23:26 +0400

    completed task 30

--- quests/quest_30_reflective_scribe.py
+++ quests/quest_30_reflective_scribe.py
@@ -1,11 +1,11 @@
-"""
+#!/usr/bin/python3"""
 Quest 30: The Reflective Scribe
 ===============================
 Level: Level 6: The Grand Challenge
 
 The Quest: Go back to three of your previous assignments and add comments (# like this) to explain what each part of your code does and why.
 
-Assigned to : <fill in your name>
+Assigned to : Ange Emmanuelle Ntsa Bineli
 Group       : group2-python-quests
 Status      : [ ] not started  [ ] in progress  [ ] done  [ ] reviewed by peers
 """
@@ -16,4 +16,6 @@ Status      : [ ] not started  [ ] in progress  [ ] done  [ ] reviewed by peers
 # and add short inline comments (#) explaining tricky lines before you push.
 # ---------------------------------------------------------------------------
 
-# TODO: implement the quest here
+#to add a comment in a code , use "#"
+comment = "#"
+print("to write a comment, use {comment}")
